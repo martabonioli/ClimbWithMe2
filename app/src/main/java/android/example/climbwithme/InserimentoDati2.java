f86f594798dbe683df288904dcd668396c8fc8bb
Your Name <you@example.com>
2020-02-11 15:42:48 +0100

    profilo + avantiCerca ++ problema all'iniizio

--- app/src/main/java/android/example/climbwithme/InserimentoDati2.java
+++ app/src/main/java/android/example/climbwithme/InserimentoDati2.java
@@ -3,6 +3,8 @@ package android.example.climbwithme;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.example.climbwithme.ui.profile.NumberPickerDialog;
+import android.example.climbwithme.ui.profile.ProfileFragment;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,6 +16,7 @@ import android.widget.ImageButton;
 import android.widget.NumberPicker;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.NetworkResponse;
@@ -32,7 +35,7 @@ import org.json.JSONObject;
 
 import java.io.UnsupportedEncodingException;
 
-public class InserimentoDati2 extends AppCompatActivity implements View.OnClickListener{
+public class InserimentoDati2 extends AppCompatActivity implements View.OnClickListener {
 
     private static final String[] paths = { "4a","4a+","4b","4b+","4c","4c+","5a","5a+","5b","5b+","5c","5c+","6a","6a+","6b","6b+","6c","6c+","7a","7a+","7b","7b+","7c","7c+","8a","8a+","8b","8b+","8c","8c+","9a","9a+","9b","9b+","9c","9c+"};
     private int livello;
@@ -44,15 +47,41 @@ public class InserimentoDati2 extends AppCompatActivity implements View.OnClickL
 
         Button addConfirm = findViewById(R.id.entra);
         addConfirm.setOnClickListener(this);
-
         ImageButton modify = findViewById(R.id.imageButton);
         modify.setOnClickListener(this);
-
-        ImageButton modify2= findViewById(R.id.imageButton2);
+        ImageButton modify2 = findViewById(R.id.imageButton2);
         modify2.setOnClickListener(this);
-
-
     }
+
+
+    /*public void showNumberPicker(View view){
+        final TextView tv = (TextView) findViewById(R.id.tv);
+        NumberPickerDialog newFragment = new NumberPickerDialog();
+        newFragment.setValueChangeListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal){
+                //Display the newly selected value from picker
+                tv.setText(matchLivello1(picker.getValue()+1));
+            }
+
+        });
+        newFragment.show(getSupportFragmentManager(), "time picker");
+    }
+
+    public void showNumberPicker2(View view){
+        final TextView tv2 = (TextView) findViewById(R.id.tv);
+        NumberPickerDialog newFragment = new NumberPickerDialog();
+        newFragment.setValueChangeListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker numberPicker, int i, int i1){
+                //Display the newly selected value from picker
+                tv2.setText(matchLivello1(numberPicker.getValue()+1));
+            }
+
+        });
+        newFragment.show(getSupportFragmentManager(), "time picker");
+    }*/
+
 
     @Override
     public void onClick(View v){
@@ -60,7 +89,17 @@ public class InserimentoDati2 extends AppCompatActivity implements View.OnClickL
             case R.id.imageButton:
                 final TextView tv = (TextView) findViewById(R.id.tv);
                 NumberPicker np = (NumberPicker) findViewById(R.id.np);
-                np.setVisibility(View.VISIBLE);
+                NumberPickerDialog newFragment = new NumberPickerDialog();
+                newFragment.setValueChangeListener(new NumberPicker.OnValueChangeListener() {
+                    @Override
+                    public void onValueChange(NumberPicker picker, int oldVal, int newVal){
+                        //Display the newly selected value from picker
+                        tv.setText(matchLivello1(picker.getValue()+1));
+                    }
+
+                });
+                newFragment.show(getSupportFragmentManager(), "time picker");
+               /* np.setVisibility(View.VISIBLE);
 
                 //Set TextView text color
                 tv.setTextColor(Color.parseColor("#3F51B5"));
@@ -89,11 +128,22 @@ public class InserimentoDati2 extends AppCompatActivity implements View.OnClickL
                         tv.setText("" + values[newVal]);
                     }
 
-                });
+                });*/
 
             break;
             case R.id.imageButton2:
-                final TextView tv2 = (TextView) findViewById(R.id.tv2);
+                final TextView tv2 = (TextView) findViewById(R.id.tv);
+                NumberPickerDialog newFragment2 = new NumberPickerDialog();
+                newFragment2.setValueChangeListener(new NumberPicker.OnValueChangeListener() {
+                    @Override
+                    public void onValueChange(NumberPicker numberPicker, int i, int i1){
+                        //Display the newly selected value from picker
+                        tv2.setText(matchLivello1(numberPicker.getValue()+1));
+                    }
+
+                });
+                newFragment2.show(getSupportFragmentManager(), "time picker");
+               /* final TextView tv2 = (TextView) findViewById(R.id.tv2);
                 NumberPicker np2 = (NumberPicker) findViewById(R.id.np2);
                 np2.setVisibility(View.VISIBLE);
 
@@ -131,7 +181,7 @@ public class InserimentoDati2 extends AppCompatActivity implements View.OnClickL
                         tv2.setText("" + values2[newVal]);
 
                     }
-                });
+                });*/
             break;
             case R.id.entra:
                 //prendi i dati inseriti e salvali sul model
@@ -234,6 +284,47 @@ public class InserimentoDati2 extends AppCompatActivity implements View.OnClickL
         if (s.equals("9b+")){ livello=34; }
         if (s.equals("9c")){ livello=35; }
         if (s.equals("9c+")){ livello=36; }
+        return livello;
+    }
+
+    public String matchLivello1(int s){
+        String livello="";
+        if (s==1){ livello="4a"; }
+        if (s==2){ livello="4a+"; }
+        if (s==3){ livello="4b"; }
+        if (s==4){ livello="4b+"; }
+        if (s==5){ livello="4c"; }
+        if (s==6){ livello="4c+"; }
+        if (s==7){ livello="5a"; }
+        if (s==8){ livello="5a+"; }
+        if (s==9){ livello="5b"; }
+        if (s==10){ livello="5b+"; }
+        if (s==11){ livello="5c"; }
+        if (s==12){ livello="5c+"; }
+        if (s==13){ livello="6a"; }
+        if (s==14){ livello="6a+"; }
+        if (s==15){ livello="6b"; }
+        if (s==16){ livello="6b+"; }
+        if (s==17){ livello="6c"; }
+        if (s==18){ livello="6c+"; }
+        if (s==19){ livello="7a"; }
+        if (s==20){ livello="7a+"; }
+        if (s==21){ livello="7b"; }
+        if (s==22){ livello="7b+"; }
+        if (s==23){ livello="7c"; }
+        if (s==24){ livello="7c+"; }
+        if (s==25){ livello="8a"; }
+        if (s==26){ livello="8a+"; }
+        if (s==27){ livello="8b"; }
+        if (s==28){ livello="8b+"; }
+        if (s==29){ livello="8c"; }
+        if (s==30){ livello="8c+"; }
+        if (s==31){ livello="9a"; }
+        if (s==32){ livello="9a+"; }
+        if (s==33){ livello="9b"; }
+        if (s==34){ livello="9b+"; }
+        if (s==35){ livello="9c"; }
+        if (s==36){ livello="9c+"; }
         return livello;
     }
 
